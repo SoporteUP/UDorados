d0f2e024f2faf1502bcdb51682c17a50e8090589
SoporteUP <soporteup2@gmail.com>
2025-07-09 14:41:46 -0600

    prospectos v2

---
+++ DoradosBlazor.Client/wwwroot/js/prospectos.js
@@ -0,0 +1,9 @@
+﻿window.slideOutCard = (id) => {
+    const card = document.querySelector(`.prospecto-id-${id}`);
+    if (card) {
+        card.classList.add('prospecto-slide-out');
+        setTimeout(() => {
+            card.classList.remove('prospecto-slide-out');
+        }, 300); // coincide con la duración en CSS
+    }
+};
